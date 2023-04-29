ff14ee7a668310772966bd4c1e1f5fa8031f89c0
jisjoe-CND <jisjoe@carnextdoor.com.au>
2023-04-29 14:26:36 +0530

    feat: add new portfolio

---
+++ lib/info.tsx
@@ -0,0 +1,18 @@
+import me from '../app/avatar.png';
+
+export const name = 'Shan Shaji';
+export const avatar = me;
+export const about = () => {
+  return (
+    <>
+      Hey, I'm a developer who loves Next.js. I work at BigCo or CoolStartup as a Software Engineer.
+    </>
+  );
+};
+export const bio = () => {
+  return (
+    <>
+      When I'm not coding, I'm probably playing video games or watching anime. I also love to travel and explore new places.
+    </>
+  );
+};
