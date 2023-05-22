8458a1f8d58ab299e22a52d73d22949388083e43
shan-shaji <shan@carnextdoor.com.au>
2023-05-23 00:00:52 +0530

    feat: add effective prompt article

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,15 @@
-import { allBlogs } from 'contentlayer/generated';
+import { allBlogs } from "contentlayer/generated";
 
 export default async function sitemap() {
   const blogs = allBlogs.map((post) => ({
-    url: `https://leerob.io/blog/${post.slug}`,
+    url: `https://shan-shaji.github.io/blog/${post.slug}`,
     lastModified: post.publishedAt,
   }));
 
-  const routes = ['', '/about', '/blog', '/guestbook', '/uses'].map(
-    (route) => ({
-      url: `https://leerob.io${route}`,
-      lastModified: new Date().toISOString().split('T')[0],
-    })
-  );
+  const routes = ["", "/about", "/blog"].map((route) => ({
+    url: `https://shan-shaji.github.io${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
 
   return [...routes, ...blogs];
 }
