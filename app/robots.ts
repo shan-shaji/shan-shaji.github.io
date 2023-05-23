315006fa3ee440700454d2a1a84c7d55616d5936
Shan Shaji <abrahamshanshaji@gmail.com>
2023-05-24 00:02:46 +0530

    feat: add effective prompt article (#2)

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,10 @@ export default function robots() {
   return {
     rules: [
       {
-        userAgent: '*',
+        userAgent: "*",
       },
     ],
-    sitemap: 'https://leerob.io/sitemap.xml',
-    host: 'https://leerob.io',
+    sitemap: "https://shan-shaji.github.io/sitemap.xml",
+    host: "https://shan-shaji.github.io",
   };
 }
